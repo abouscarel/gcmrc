a793703608016e7f016ac50ddf2d7d6db776d381
Antonin Bouscarel <antonin@mtllfsdk3syf2fs.cbc-rc.ca>
2021-12-02 10:29:48 -0500

    MNCMS-XXXX - Form - Change form input

--- index.js
+++ index.js
@@ -1,14 +1,98 @@
 const inquirer = require('inquirer');
+const shell = require('shelljs');
+
+const getJiraCode = async () => {
+  const branchName = shell.exec("git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ \1/'", {silent: true}).stdout
+  let splittedBranchName = branchName.split('/')
+  if (splittedBranchName.length < 2) {
+    console.log('Nom de branch incorrect.\nLe format doit être : <Type de story>/<Projet>-<Numero de la story>-<Description>')
+    process.exit()
+  }
+  splittedBranchName = splittedBranchName[splittedBranchName.length - 1].split('-')
+  let jiraCode = `${splittedBranchName[0]}-${splittedBranchName[1]}`
+  const prompt = inquirer.createPromptModule();
+  let answer = await prompt({
+    type: 'confirm',
+    name: 'item',
+    message: `Le Code item Jira est il correct ? ${jiraCode}`,
+    default: true
+  })
+
+  if (answer.item === false) {
+    let jiraCodeAnswer = await prompt({
+      type: 'input',
+      name: 'jiraCode',
+      message: `Entrez votre Code Item Jira ?`,
+    })
+    if (!jiraCodeAnswer.jiraCode) {
+      console.log('Code Jira incorrect.')
+      process.exit()
+    }
+    jiraCode = jiraCodeAnswer.jiraCode
+  }
+  return jiraCode
+}
+
+const getSection = async () => {
+  const prompt = inquirer.createPromptModule();
+  let section = 'N/C'
+  let sectionAnswer = await prompt({
+    type: 'input',
+    name: 'section',
+    message: `Quel est la section impacté ?`,
+  })
+  if (sectionAnswer.section) {
+    section = sectionAnswer.section
+  }
+  return section
+}
+
+const getCommitMessage = async () => {
+  const prompt = inquirer.createPromptModule();
+  let commitMessage = null
+  let commitMessageAnswer = await prompt({
+    type: 'input',
+    name: 'commitMessage',
+    message: `Message de commit :`,
+  })
+  if (commitMessageAnswer.commitMessage) {
+    commitMessage = commitMessageAnswer.commitMessage
+  } else {
+    console.log('Message de commit incorrect.')
+    process.exit()
+  }
+  return commitMessage
+}
 
 const start = async () => {
   const prompt = inquirer.createPromptModule();
-  const answer = await prompt({
-    type: 'confirm',
-    name: 'item',
-    message: 'Jira code name is correct ?',
-  })
+  if (!shell.which('git')) {
+    shell.echo('Sorry, this script requires git');
+    shell.exit(1);
+  }
+  
+  const jiraCode = await getJiraCode()
+  const section = await getSection()
+  const commitMessage = await getCommitMessage()
 
-  console.log('answer', answer)
+  // let answer = await prompt({
+  //   type: 'list',
+  //   message: 'Selectionnez une commande',
+  //   name: 'list',
+  //   choices: [
+  //     { name: 'npm', value: 'cmd_1' },
+  //     { name: 'yarn', value: 'cmd_2' },
+  //   ],
+  // });
+  // console.log('Answer:', answer);
+  
+  //console.log('\ncommit message :', `${jiraCode} - ${section} - ${commitMessage}`)
+
+  if (jiraCode && section && commitMessage) {
+    shell.exec(`git commit -m "${jiraCode} - ${section} - ${commitMessage}"`)
+  } else {
+    console.log('error')
+  }
 }
 
 start()
